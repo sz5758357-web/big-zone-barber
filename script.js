37f9749a6eb0d63e82194fe07897274661ebee2d
sz5758357-web <sz5758357@gmail.com>
2026-07-22 16:23:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 window.addEventListener("scroll",()=>{
 
-const nav=document.querySelector("nav");
+let nav=document.querySelector("nav");
+
 
 if(window.scrollY>50){
 
 nav.style.background="#000";
 
-}else{
+}
 
-nav.style.background="#050505cc";
+else{
+
+nav.style.background="#000";
 
 }
+
 
 });
