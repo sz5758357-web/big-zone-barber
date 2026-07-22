16b9e0bd4fa11dd831abef70549722187a3f0cb5
sz5758357-web <sz5758357@gmail.com>
2026-07-22 16:19:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll",()=>{
+
+const nav=document.querySelector("nav");
+
+if(window.scrollY>50){
+
+nav.style.background="#000";
+
+}else{
+
+nav.style.background="#050505cc";
+
+}
+
+});
